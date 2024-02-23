7d94178a6c51307d341b9ac8731d4a49f237ce90
Guihal <guihalbonik@gmail.com>
2024-02-23 16:24:40 +0300

    fdf

--- gsap/main copy.js
+++ gsap/main copy.js
@@ -72,7 +72,7 @@ const menuOpen = gsap.timeline({ paused: "true", reversed: "true" });
     } else if(window.innerWidth < 1920){
       return num * 2;
     } else if(window.innerWidth < 768){
-      return num * 8;
+      return num * 15;
     } else{
       return  num;
     }
